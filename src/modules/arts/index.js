34580cb3f6810825cc2eb7662c3208bb0860ad64
Omci <melloulchi.omar@gmail.com>
2023-06-09 23:16:53 +0200

    arts basic routes finished

--- src/modules/arts/index.js
+++ src/modules/arts/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
-const { getAllArt } = require('./controller')
+const { getAllArt, getOneArt, createArt, putOneArt, deleteArt} = require('./controller')
 
 router.get('/', getAllArt)
+router.get('/:id', getOneArt)
+router.post('/', createArt)
+router.put('/:id', putOneArt)
+router.delete('/:id', deleteArt)
 
 module.exports = router
--- src/modules/arts/model.js
+++ src/modules/arts/model.js
@@ -11,4 +11,53 @@ const findAllArt = () => {
         })
 }
 
-module.exports = { findAllArt };
+const findOneArt = (id) => {
+    return db
+        .query('select * from art where id = ?', [id])
+        .then(([data]) => {
+            return data
+        })
+        .catch((err) => {
+            console.error('err', err)
+        })
+}
+
+
+const addArt = (art) => {
+    const { ref_img , full_title, short_title, achievement_date, dimensions, art_number, description, url_origin, img_url, author_id, category_id, technique_id } = art
+    return db 
+        .query("INSERT INTO art (ref_img , full_title, short_title, achievement_date, dimensions, art_number, description, url_origin, img_url, author_id, category_id, technique_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [ref_img , full_title, short_title, achievement_date, dimensions, art_number, description, url_origin, img_url, author_id, category_id, technique_id])
+        .then(([data]) => {
+            return { id: data.insertId, ...art}
+        })
+        .catch((err) => {
+            console.error(err); })
+}
+
+
+const modifyOneArt = (art, artId)=>{
+    return db
+    .query('update art set ? where id = ?', [art, artId])
+    .then(([result])=>{
+        return result
+    })
+    .catch((err)=>{
+        console.error('err', err)
+    })
+}
+
+
+const removeArt = (id) => {
+    return db
+        .query("delete from art where id = ?", [id])
+        .then(([data]) => {
+            return data;
+        })
+        .catch((err) =>{
+            console.error("Error ", err)
+        })
+} 
+
+
+module.exports = { findAllArt, findOneArt, addArt, modifyOneArt, removeArt};
