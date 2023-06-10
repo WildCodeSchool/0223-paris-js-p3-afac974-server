376f3b58e07b12bc174e389dbd9cb3a2bfa7a441
ornel77 <t.ornel@yahoo.fr>
2023-06-10 14:10:38 +0200

    get, getById and post for authors ok

--- src/modules/authors/controller.js
+++ src/modules/authors/controller.js
@@ -20,7 +20,7 @@ const getOneAuthor = (req, res) => {
             if (data) {
                 res.json(data)
             } else {
-                res.status(404).json({ message: "No author found with this id"})
+                res.status(404).json({ message: "No author found with this id!"})
             }
         })
         .catch((err) => res.status(500).json({ message : "Server error"}))
