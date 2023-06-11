d0cf27f2262ae420896dcfbaf14fe979caf6fb36
Omci <melloulchi.omar@gmail.com>
2023-06-11 22:05:19 +0200

    users validator routed

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 const { getAll, getOneUser, putOneUser, createUser,deleteUser } = require("./controller")
+const { validateAddUser } = require("./validator")
 
 router.get('/', getAll)
 router.get('/:id', getOneUser )
-router.post('/', createUser)
+router.post('/', validateAddUser, createUser)
 router.put('/:id', putOneUser)
 router.delete('/:id', deleteUser)
 
