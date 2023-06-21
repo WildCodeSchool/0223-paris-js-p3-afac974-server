f68e79b07639ea95b2d123ef870fb3e38a359688
Omci <melloulchi.omar@gmail.com>
2023-06-21 16:39:20 +0200

    user favorite done

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 
-const { getAll, getOneUser, putOneUser, register,deleteUser, login, logout } = require("./controller")
+const { getAll, getOneUser, putOneUser, register,deleteUser, login, logout, addUserFavorite, getAllFavoriteById } = require("./controller")
 
 const { validateAddUser } = require("./validator")
 const  { hashPassword, isAdmin, authorization } = require('../../middleware/auth')
 
 router.get('/', authorization, isAdmin, getAll)
 router.get("/logout", authorization, logout)
+router.get("/favorite/:id", getAllFavoriteById )
 router.get('/:id', authorization, isAdmin, getOneUser )
 router.post('/register', validateAddUser, hashPassword, register)
 router.post("/login", login)
+router.post("/favorite/:user_id/:art_id", addUserFavorite)
 router.put('/:id', authorization, validateAddUser, hashPassword, putOneUser)
 router.delete('/:id', authorization, isAdmin, deleteUser)
 
