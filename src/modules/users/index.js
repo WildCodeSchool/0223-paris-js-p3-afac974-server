160842697bf976b1e9be155f4b1dbd074c419b58
Omci <melloulchi.omar@gmail.com>
2023-06-21 13:51:54 +0200

    added middlewares into getOne and put and delete, tested

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -7,10 +7,10 @@ const  { hashPassword, isAdmin, authorization } = require('../../middleware/auth
 
 router.get('/', authorization, isAdmin, getAll)
 router.get("/logout", authorization, logout)
-router.get('/:id', getOneUser )
+router.get('/:id', authorization, isAdmin, getOneUser )
 router.post('/register', validateAddUser, hashPassword, register)
 router.post("/login", login)
-router.put('/:id', validateAddUser, hashPassword, putOneUser)
-router.delete('/:id', deleteUser)
+router.put('/:id', authorization, validateAddUser, hashPassword, putOneUser)
+router.delete('/:id', authorization, isAdmin, deleteUser)
 
 module.exports = router;
