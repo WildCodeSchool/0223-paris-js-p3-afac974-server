ae4c21ce61be4ad1cf92bca7ef5d76675965655f
Omci <melloulchi.omar@gmail.com>
2023-07-05 10:32:44 +0200

    updated file path of db

--- migrate.js
+++ migrate.js
@@ -17,7 +17,7 @@ const migrate = async () => {
     await connection.query(`create database ${DB_NAME}`);
     await connection.query(`use ${DB_NAME}`);
 
-    const sql = fs.readFileSync("./afac974.sql", "utf8");
+    const sql = fs.readFileSync("./afac974_db.sql", "utf8");
 
     await connection.query(sql);
 
