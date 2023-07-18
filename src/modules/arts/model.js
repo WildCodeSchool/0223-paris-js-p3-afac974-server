f5113a7587ff2d84be838bc55c4914a837b7cfc1
anohjr <rosenaomi.hj@gmail.com>
2023-07-18 15:58:25 +0200

    indentation code getAll function for db

--- src/modules/arts/model.js
+++ src/modules/arts/model.js
@@ -2,35 +2,38 @@ const { query } = require('../../config/database');
 const db = require('../../config/database')
 
 const findAllArt = (queryParams) => {
-    console.log(queryParams)
-    let keyOrder = queryParams.order   // extraction du filtre ORDER
-    if(typeof keyOrder === "undefined") keyOrder = "DESC";
-    console.log("keyOrder:", keyOrder)
-    delete queryParams.order;   // suppression de la clé "order"
-    let queryOrder = 'ORDER by id ' + keyOrder; 
-    console.log("queryOrder", queryOrder)
+    let keyOrder = queryParams.order; // extraction du filtre ORDER
+    if (typeof keyOrder === "undefined") keyOrder = "DESC";
+    delete queryParams.order; // suppression de la clé "order"
+    let queryOrder = 'ORDER by id ' + keyOrder;
     let queryFilters = "";
     let arrayFilters = [];
     let params = [];
     for (let key in queryParams) {
-        arrayFilters.push(`${key} = ?`);
-        params.push(queryParams[key])
+      arrayFilters.push(`${key} = ?`);
+      params.push(queryParams[key]);
     }
-    params.push(keyOrder)
+    params.push(keyOrder);
     queryFilters = arrayFilters.join(" AND ");
     if (queryFilters.length > 0) queryFilters = " WHERE " + queryFilters;
-    console.log("db text:", queryFilters)
-
+  
     return db
-        .query(`select * FROM art as a JOIN category as c on a.category_id = c.c_id JOIN technique as t on a.technique_id  
-        = t.t_id JOIN author on author.aut_id = a.author_id${queryFilters} ${queryOrder}`, params)
-        .then(([data]) => {
-            return data
-        })
-        .catch((err) => {
-            console.error('err', err)
-        })
-}
+      .query(
+        `SELECT * FROM art AS a 
+         JOIN category AS c on a.category_id = c.c_id 
+         JOIN technique AS t on a.technique_id = t.t_id 
+         JOIN author ON author.aut_id = a.author_id
+         ${queryFilters} ${queryOrder}`,
+        params
+      )
+      .then(([data]) => {
+        return data;
+      })
+      .catch((err) => {
+        console.error('err', err);
+      });
+  };
+  
 
 const findOneArt = (id) => {
     return db
